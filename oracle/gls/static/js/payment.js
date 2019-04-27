5db0236d6dca55eb4853a619b9abcacef0dcf964
Vijayasekar Natarajan <nvijayasekar@gmail.com>
2019-04-28 02:13:21 +0530

    Added Testing Json data and Div for tooltip validation.

--- oracle/gls/static/js/payment.js
+++ oracle/gls/static/js/payment.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
-	$.getJSON('static/json/guide.json', function(jd) {
-		result = jd.steps;
+	$.getJSON('static/json/guide.json', function(data) {
+		result = data.steps;
 		$.each(result,function(id,item){
 			var getData = "id : "+result[id].id+",																			content:"+result[id].content+",																				selector:"+result[id].selector+" ,																				Next: "+result[id].next+"";
 			
