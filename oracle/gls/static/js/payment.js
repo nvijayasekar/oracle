c14b319363f7f786c862d03738d2907d3f494d95
Vijayasekar Natarajan <nvijayasekar@gmail.com>
2019-04-28 01:01:55 +0530

    Add Tootltip function for displaying json data for each div on hover.

--- oracle/gls/static/js/payment.js
+++ oracle/gls/static/js/payment.js
@@ -9,8 +9,40 @@ $(document).ready(function() {
 	  
 	});
 
-	
+	$('div').on('click',function(){
+		
+		$(this).next().show();
+	});
+var a = $('div'),
+    tip, text,
+    base = document.createElement('tooltip'); //Defining all objects
 
+for (var x=0;x < a.length;x++) { //I'm using "for" loop to get all "a" elements with attribute "tooltip".
+     a[x].onmouseover = function () {
+         text = this.getAttribute('tooltip');
+         tip = document.createTextNode(text);
+         if (text != null) {// Checking if tooltip is empty or not.
+		 
+               base.innerHTML = '';
+               base.appendChild(tip);
+               if (document.getElementsByTagName('tooltip')[0]){// Checking for any "tooltip" element
+                   document.getElementsByTagName('tooltip')[0].remove();// Removing old tooltip
+               }
+               base.style.top = (event.pageY + 20) + 'px';
+               base.style.left = (event.pageX + 20) + 'px';
+			   base.style.backgroundColor = '#DC143C';
+			   base.style.color = 'white';
+			    base.style.height = '20%';
+				 base.style.width = '20%';
+               document.body.appendChild(base);
+         }
+
+     };
+     a[x].onmouseout = function () {
+         document.getElementsByTagName('tooltip')[0].remove();// Remove last tooltip
+     };
+}
+    
 
 
 
