dd5011d562a4b81d3fdc8a8f37a3e5b57fe7c81e
Vijayasekar Natarajan <nvijayasekar@gmail.com>
2019-04-28 11:36:02 +0530

    Code alignment fixed.

--- oracle/gls/static/js/player.js
+++ oracle/gls/static/js/player.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	
+	
+	//Json Script Code starts here - Fetch Json data to render html page.
 	$.getJSON('static/json/guide.json', function(data) {
 		result = data.steps;
 		$.each(result,function(id,item){
@@ -8,54 +10,54 @@ $(document).ready(function() {
 			$(result[id].selector).attr( 'tooltip',getData);	
 	  });
 	  
-	});
-
+	}); // End Json Script 
+	
+	//Event to trigger next div to show 
 	$('div').on('click',function(){
 		
 		$(this).next().show(); //Current div action
 	});
 	
+	//Reset or Close child div
 	$('#reset').on('click',function(e){
 		e.preventDefault();
 		$('div').hide(); 
 		$('.myClass1').show();
 	});
 	
-var a = $('div'),
+	//Tooltip Display Info Function - Code starts here
+	var a = $('div'),
     tip, text,
     base = document.createElement('tooltip'); //Defining all objects
 
-for (var x=0;x < a.length;x++) { //I'm using "for" loop to get all "a" elements with attribute "tooltip".
-     a[x].onmouseover = function () {
-         text = this.getAttribute('tooltip');
-         tip = document.createTextNode(text);
-         if (text != null) {// Checking if tooltip is empty or not.
+	for (var x=0;x < a.length;x++) { //using "for" loop to get all "div" elements with attribute "tooltip".
+	
+		a[x].onmouseover = function () { // Mouse hover code to display tooltip
+        
+			text = this.getAttribute('tooltip');
+			tip = document.createTextNode(text);
+        
+			if (text != null) {// Checking if tooltip is empty or not.
 		 
-               base.innerHTML = '';
-               base.appendChild(tip);
-               if (document.getElementsByTagName('tooltip')[0]){// Checking for any "tooltip" element
+				base.innerHTML = '';
+				base.appendChild(tip);
+				if (document.getElementsByTagName('tooltip')[0]){// Checking for any "tooltip" element
                    document.getElementsByTagName('tooltip')[0].remove();// Removing old tooltip
-               }
-               base.style.top = (event.pageY + 20) + 'px';
-               base.style.left = (event.pageX + 20) + 'px';
-			   base.style.backgroundColor = '#DC143C';
-			   base.style.color = 'white';
-			    base.style.height = '20%';
-				 base.style.width = '20%';
-				
-               document.body.appendChild(base);
-         }
+				}
+				base.style.top = (event.pageY + 20) + 'px';
+				base.style.left = (event.pageX + 20) + 'px';
+				base.style.backgroundColor = '#DC143C';
+				base.style.color = 'white';
+				base.style.height = '20%';
+				base.style.width = '20%';
+				document.body.appendChild(base);
+			}
 
-     };
-     a[x].onmouseout = function () {
+		};
+		//Tooltip Remove on Mouseout Function
+		a[x].onmouseout = function () {
          document.getElementsByTagName('tooltip')[0].remove();// Remove last tooltip
-     };
-}
-    
+		};
+	}  // End For Loop function
 
-
-
-	
-	
-	
- });
+}); // End Script
--- oracle/gls/views.py
+++ oracle/gls/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
+# Render Html Page by requesting (http://127.0.0.1:8000/).
 def index(request):
 	return render(request, 'index.html', {})
